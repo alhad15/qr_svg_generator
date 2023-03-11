fba59e725437cc13d3f49d2e15a64ff0cb0b35d9
Alhad Parkhi <alhad15@users.noreply.github.com>
2023-03-11 12:51:05 +0530

    Update qr_svg_image.dart

--- lib/src/qr_svg_image.dart
+++ lib/src/qr_svg_image.dart
@@ -36,9 +36,6 @@ class QrSvgImage {
     final str =
         '<svg id="mainLayer" xmlns="http://www.w3.org/2000/svg" width="$size" height="$size" viewBox="0 0 $size $size">\n'
         '  <defs>\n'
-        '    <style>\n'
-        '$colors\n'
-        '    </style>\n'
         '  </defs>\n'
         '  <title>$title</title>\n'
         '${rows.toString()}'
